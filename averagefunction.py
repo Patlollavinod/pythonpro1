d7f2d7b1444c95538913e75e36446b317dd758bb
sachin <sachin@example.com>
2019-10-18 09:47:56 +0530

    Committed by Sachin@example.com

---
+++ averagefunction.py
@@ -0,0 +1,9 @@
+def avg(x,y):
+	print("x=",x)
+	print("y=",y)
+	return (x+y)/2
+
+x=int(input("Please enter the number1: "))
+y=int(input("Please enter the number2: "))
+
+print("Average is ", avg(x,y))
